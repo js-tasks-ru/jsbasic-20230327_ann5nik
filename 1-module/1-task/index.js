4da41e25552bdcb8ccec150e34c3d6c07eef1dc5
Anna Gorepyakina <annagorepyakina@gmail.com>
2023-04-02 17:07:06 +0300

    ошибка тестов

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,13 @@
 function factorial(n) {
-  // ваш код...
+  let result = n;
+  if (n === 0 || n === 1) {
+    console.log(1);
+  } else if (n < 0){
+    console.log("Факториал может вычисляться только с натуральными положительными числами");
+  } else {
+    for (let i = 1; i < n; i++) {
+      result = result * i;
+    }
+    console.log(result);
+  }
 }
