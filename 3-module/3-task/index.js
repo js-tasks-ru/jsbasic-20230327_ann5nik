3ea63e439f75adda9d1c05a8b0376036bf2f40b0
Anna Gorepyakina <annagorepyakina@gmail.com>
2023-04-13 12:17:58 +0300

    сделана третья задача

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,10 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('-');
+  return arr.map((item, index) => {
+      if (index == 0) {
+        return item;
+      } else {
+        return item[0].toUpperCase() + item.slice(1);
+      }
+  }).join('');
 }
