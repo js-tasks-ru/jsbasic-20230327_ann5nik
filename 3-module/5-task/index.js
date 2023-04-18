28ffce37b6e8627f2ea5a67a8020ada1ab68352c
Anna Gorepyakina <annagorepyakina@gmail.com>
2023-04-18 15:00:07 +0300

    сделаны все задачи модуля

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,9 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str.split(" ");
+  let newArr = arr.filter(item => +item);
+  let result = {
+    min: Math.min.apply(null, newArr),
+    max: Math.max.apply(null, newArr)
+  }
+  return result;
 }
